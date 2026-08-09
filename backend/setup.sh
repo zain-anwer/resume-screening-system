e08b43342ccd3b6a92b315d2e28ad741786cde8f
zain <zain.anwer192005@gmail.com>
2026-08-09 15:41:47 +0500

    Edited setup.sh

--- backend/setup.sh
+++ backend/setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+set -e
 
-# create and activate virtual environment it it doesn't exist
+# navigate to the backend and create and activate virtual environment it it doesn't exist
+cd backend
 [ -d ".venv" ] || python -m venv .venv
 source .venv/Scripts/activate
 
@@ -10,3 +12,10 @@ python -m pip install -r requirements.txt
 
 # install spacy model
 python -m spacy download en_core_web_sm
+
+# navigate to the frontend and install dependencies
+cd ../frontend
+npm install
+
+# navigate back to the root directory
+cd ..
